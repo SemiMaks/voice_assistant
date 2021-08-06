79bc5205bfa586e977a69f077975cb4d3963aaa2
semimaks <semimaks@mail.ru>
2021-08-07 00:52:13 +0300

    Правка кода

--- app/assistant.py
+++ app/assistant.py
@@ -1,8 +1,4 @@
-#
 # Создание голосового ассистента
-#
-# Подключаем необходимые библиотеки,
-# предварительно установленные с pypi.org
 #
 
 import speech_recognition as sr  # преобразование голоса (отправляется на сервера для распознавания)
@@ -16,14 +12,14 @@ from gtts import gTTS  # преобразование текста в аудио
 # Ассистент слушает
 def listen():
     voice_recognizer = sr.Recognizer()
-
     with sr.Microphone() as source:
         print("Скажите что-нибудь >>> ")
-        audio = voice_recognizer.listen(source)
+        # voice_recognizer.adjust_for_ambient_noise(source, duration=5)  # Убираем шум
+        audio = voice_recognizer.listen(source)  # слушаем микрофон
 
     try:
+        print("Google..." + voice_recognizer.recognize_google(audio))
         voice_text = voice_recognizer.recognize_google(audio, language="ru")
-        # text = input("Скажите что-нибудь >>> ")
         print(f"Вы сказали: {voice_text}")
         return voice_text
     except sr.UnknownValueError:
@@ -36,10 +32,14 @@ def listen():
 def say(text):
     voice = gTTS(text, lang="ru")
     unique_file = "audio_" + str(random.randint(0, 10000)) + ".mp3"  # audio_10.mp3
+    # unique_file = "audio_" + str(1) + ".mp3"
+    # voice.save('song.mp3')
     voice.save(unique_file)
-
     playsound.playsound(unique_file)
+    # playsound('song.mp3')
+    print(voice)
     os.remove(unique_file)
+    # os.remove('song.mp3')
 
     print(f"Ассистент: {text}")
 
@@ -47,10 +47,15 @@ def say(text):
 # Обработка текста введенного с клавиатуры
 def handle_command(command):
     command = command.lower()  # Переводим в нижний регистр все слова
+    print('Слово выглядит так:', command)
 
-    if command == "Привет":
-        say("Привет-привет!")
-    elif command == "Пока":
+    if command == "привет":
+        say("привет привет")
+    elif command == "тест":
+        say("запуск теста")
+    elif command == "повтори":
+        say("повтор команды")
+    elif command == "пока":
         stop()
     else:
         say("Не понятно, повторите")
@@ -68,12 +73,13 @@ def start():
 
     while True:  # бесконечный цикл
         command = listen()
+        # command = 'привет'
         handle_command(command)
 
 
 # Пробуем запустить ассистента
 try:
     start()
-    # handle_command('привет')
+
 except KeyboardInterrupt:
     stop()
---
+++ app/pl_sound.py
@@ -0,0 +1,3 @@
+from playsound import playsound
+
+playsound('E:/WORKS_PYTHON/voice_assistant/app/Harmattan.mp3')
